a90eb33fab140f0f3b0c2cca7b7fd2c6b7c827fa
nam hoang <nam.hoang@ntq-solution.com.vn>
2021-01-08 10:45:29 +0700

    [~] cc

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,28 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.post('/register', passport.authenticate('local-register', {
-  successRedirect: '/login',
-  failureRedirect: '/register',
-  failureFlash: true
-}))
+router.post(
+  '/register',
+  passport.authenticate('local-register', {
+    successRedirect: '/login',
+    failureRedirect: '/register',
+    failureFlash: true
+  }),
+  (req, res, next) => {
+    // user has been authenticated and been attached to current session
+  }
+)
 
-router.post('/login', passport.authenticate('local-login', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
+router.post(
+  '/login',
+  passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }),
+  (req, res, next) => {
+    // user has been authenticated and been attached to current session
+  }
+)
 
 module.exports = router;
